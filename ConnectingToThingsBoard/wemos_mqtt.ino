691b0c471e590a72d76fbe7735c9943c61e2257b
andre <andre.roussel@ccnb.ca>
2020-02-24 16:15:40 -0400

    Added since we needed to use the wemos D1 R1 clone

---
+++ ConnectingToThingsBoard/wemos_mqtt.ino
@@ -0,0 +1,89 @@
+
+#include <ESP8266WiFi.h>
+#include <ThingsBoard.h>
+
+#define WIFI_AP "Objets_IDO"
+#define WIFI_PASSWORD "42Bidules!"
+
+#define TOKEN "mtJOKgDaZmp399mtRiMK"
+
+
+char thingsboardServer[] = "192.168.2.4";
+
+WiFiClient wifiClient;
+
+
+
+ThingsBoard tb(wifiClient);
+
+int status = WL_IDLE_STATUS;
+unsigned long lastSend;
+
+void setup()
+{
+  Serial.begin(9600);
+  
+  delay(10);
+  InitWiFi();
+  lastSend = 0;
+}
+
+void loop()
+{
+  if ( !tb.connected() ) {
+    reconnect();
+  }
+
+  if ( millis() - lastSend > 1000 ) { // Update and send only after 1 seconds
+    getAndSendTemperatureAndHumidityData();
+    lastSend = millis();
+  }
+
+  tb.loop();
+}
+
+void getAndSendTemperatureAndHumidityData()
+{
+  
+  
+
+  tb.sendTelemetryFloat("time", millis());
+  
+}
+void InitWiFi()
+{
+  Serial.println("Connecting to AP ...");
+  // attempt to connect to WiFi network
+
+  WiFi.begin(WIFI_AP, WIFI_PASSWORD);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("Connected to AP");
+}
+
+
+void reconnect() {
+  // Loop until we're reconnected
+  while (!tb.connected()) {
+    status = WiFi.status();
+    if ( status != WL_CONNECTED) {
+      WiFi.begin(WIFI_AP, WIFI_PASSWORD);
+      while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print(".");
+      }
+      Serial.println("Connected to AP");
+    }
+    Serial.print("Connecting to ThingsBoard node ...");
+    if ( tb.connect(thingsboardServer, TOKEN) ) {
+      Serial.println( "[DONE]" );
+    } else {
+      Serial.print( "[FAILED]" );
+      Serial.println( " : retrying in 5 seconds]" );
+      // Wait 5 seconds before retrying
+      delay( 5000 );
+    }
+  }
+}
